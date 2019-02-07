815a2cdee4ff7ac2a5fc5ae0256ebbfba4399afb
Zane Waite <zmwaite182@stevenscollege.edu>
2019-02-07 10:05:54 -0500

    Added init function to Item class

--- item.py
+++ item.py
@@ -1,6 +1,10 @@
 # Item Class
 
+import datetime
+
 class Item(object):
 
-    def __init__(self):
-        
+    def __init__(self, text):
+        self.time = datetime.datetime.now()
+        self.completed = False
+        self.text = text
